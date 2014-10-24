6ac916a170a4b25a427872e302cad96038befbd8
galedric <g@ledric.me>
2014-10-25 00:39:53 +0200

    Missing part of last commit

--- app/gt/Global.scala
+++ app/gt/Global.scala
@@ -1,9 +1,11 @@
 package gt
 
 import play.libs.Akka
+import scala.sys.process._
 
 import scala.concurrent.ExecutionContext
 
 object Global {
 	implicit lazy val ExecutionContext: ExecutionContext = Akka.system.dispatchers.lookup("default-pool")
+	val serverVersion = "git rev-parse HEAD".!!.trim
 }
