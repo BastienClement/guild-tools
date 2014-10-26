47b3595cbb1670ae0a997760223c0dcd5820b487
galedric <g@ledric.me>
2014-10-26 19:27:11 +0100

    Use GUID as primary feed key

--- app/models/Feed.scala
+++ app/models/Feed.scala
@@ -6,21 +6,20 @@ import gt.Socket
 import api._
 import java.sql.Timestamp
 
-case class Feed(id: Int, guid: String, source: String, title: String, link: String, time: Timestamp, tags: String)
+case class Feed(guid: String, source: String, title: String, link: String, time: Timestamp, tags: String)
 
 /**
  * Answers database
  */
 class Feeds(tag: Tag) extends Table[Feed](tag, "gt_feed") {
-	def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-	def guid = column[String]("guid")
+	def guid = column[String]("guid", O.PrimaryKey)
 	def source = column[String]("source")
 	def title = column[String]("title")
 	def link = column[String]("link")
 	def time = column[Timestamp]("time")
 	def tags = column[String]("tags")
 
-	def * = (id, guid, source, title, link, time, tags) <> (Feed.tupled, Feed.unapply)
+	def * = (guid, source, title, link, time, tags) <> (Feed.tupled, Feed.unapply)
 }
 
 /**
