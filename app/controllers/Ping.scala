f10e06b6b94fcb63d71d466cdac1df5247fca595
galedric <g@ledric.me>
2015-01-12 01:05:40 +0100

    Implement basic Ping API security

--- app/controllers/Ping.scala
+++ app/controllers/Ping.scala
@@ -1,11 +1,24 @@
 package controllers
 
 import api.DashboardHandler
+import play.api.Play._
 import play.api.mvc.{Action, Controller}
 
 object Ping extends Controller {
-	def dashboardFeed = Action {
+	private val secret = current.configuration.getString("ping.secret") getOrElse ""
+	private val sources = current.configuration.getStringSeq("ping.sources") getOrElse Nil
+
+	private def PingAction(execute: => Unit) = Action { request =>
+		val key = request.queryString.getOrElse("key", Nil)
+		if (key.size > 0 && key(0) == secret && sources.contains(request.remoteAddress)) {
+			execute
+			Ok("OK")
+		} else {
+			Forbidden("NOT OK")
+		}
+	}
+
+	def dashboardFeed = PingAction {
 		DashboardHandler.Feed.refresh()
-		Ok("")
 	}
 }
