760ee8dc1761a1808ad3d441f0a8cfd959b5117a
galedric <g@ledric.me>
2015-10-27 17:46:17 +0100

    Automatic Typescript compiler when running in Dev mode

--- app/gt/GuildTools.scala
+++ app/gt/GuildTools.scala
@@ -20,4 +20,38 @@ object GuildTools {
 }
 
 class GuildTools @Inject() (lifecycle: ApplicationLifecycle) {
+	Play.mode match {
+		case Mode.Dev =>
+			setupTypescriptCompiler()
+
+		case Mode.Prod =>
+			setupCharacterRefresher()
+	}
+
+	def stopHook(fn: => Unit): Unit = lifecycle.addStopHook(() => Future.successful(fn))
+
+	def setupTypescriptCompiler(): Unit = {
+		val path = Play.configuration.getString("dev.path").get
+		val pwd = Play.configuration.getString("dev.dir").get
+		val tsc = Play.configuration.getString("dev.tsc").get
+
+		println("TSC   - Starting Typescript compiler...")
+		val process = Process(Seq(tsc, "-w"), new File(s"$pwd/public/tools"), "PATH" -> path) run ProcessLogger { line =>
+			println("TSC   - " + line)
+		}
+
+		stopHook { process.destroy() }
+	}
+
+	def setupCharacterRefresher(): Unit = {
+		// Char update job
+		/*scheduler.schedule(1500.seconds, 15.minutes) {
+			val chars = RosterService.chars.values.toSeq.view
+			chars.filter(c => c.active && !c.invalid && c.last_update < Platform.currentTime - 21600000)
+				.sortBy(_.last_update)
+				.take(5)
+				.map(_.id)
+				.foreach(RosterService.refreshChar)
+		}*/
+	}
 }
