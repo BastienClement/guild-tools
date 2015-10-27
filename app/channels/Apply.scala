be9c9705054465e5ffc34c18e41750a407a510c2
galedric <g@ledric.me>
2015-10-27 10:16:20 +0100

    Apply cleanup

--- app/channels/Apply.scala
+++ app/channels/Apply.scala
@@ -25,12 +25,14 @@ class Apply(user: User) extends ChannelHandler {
 	}
 
 	// List of open applys that the user can access
-	request("open-list") { p => Applys.openForUser(user.id, user.member, user.promoted).result.run }
+	request("open-list") { p => Applys.openForUser(user).result.run }
 
 	// Load a specific application data
 	request("apply-data") { p =>
 		val id = p.value.as[Int]
-		for (apply <- Applys.filter(_.id === id).headOption) yield apply.filter(a => Applys.canAccess(a.user, a.stage, user))
+		for (apply <- Applys.filter(_.id === id).headOption) yield {
+			apply.filter(a => Applys.canAccess(a.user, a.stage, user))
+		}
 	}
 
 	// Request the message feed and body
@@ -39,20 +41,19 @@ class Apply(user: User) extends ChannelHandler {
 		val query = for {
 			(owner, stage, body) <- Applys.filter(_.id === id).map(a => (a.user, a.stage, a.data)).result.head
 			_ = if (Applys.canAccess(owner, stage, user)) () else throw new Exception("Access to this application is denied")
-			feed <- ApplyFeed.forApply(id, user.member).result
+			feed <- Applys.feedForApply(id, user.member).result
 		} yield (feed, body)
 		query.run
 	}
 
 	// Update the unread status for an application
-	message("set-seen") { p => Applys.markAsRead(p.value.as[Int], user) }
+	message("set-seen") { p => Applys.markAsRead(user, p.value.as[Int]) }
 
 	// Post a new message in an application
 	request("post-message") { p =>
 		val apply = p("apply").as[Int]
 		val body = p("message").as[String]
 		val secret = p("secret").as[Boolean]
-		if (secret && !user.member) throw new Exception("Apply cannot posts private messages")
-		Applys.postMessage(user, apply, body, secret) map (_ => true)
+		for (_ <- Applys.postMessage(user, apply, body, secret)) yield true
 	}
 }
--- app/models/Apply.scala
+++ app/models/Apply.scala
@@ -2,9 +2,9 @@ package models
 
 import java.sql.Timestamp
 import models.mysql._
+import reactive.ExecutionContext
 import scala.util.Success
 import utils.{PubSub, SmartTimestamp}
-import reactive.ExecutionContext
 
 // ============================================================================
 
@@ -47,8 +47,9 @@ object Applys extends TableQuery(new Applys(_)) with PubSub[User] {
 		else false
 	}
 
-	// Fetch every open applications visible by the user
-	val openForUser = Compiled((user: Rep[Int], member: Rep[Boolean], promoted: Rep[Boolean]) => {
+	// Fetch every open applications visible by the user and their unread status
+	def openForUser(user: User) = openForUserQuery(user.id, user.member, user.promoted)
+	private val openForUserQuery = Compiled((user: Rep[Int], member: Rep[Boolean], promoted: Rep[Boolean]) => {
 		val default_read = SmartTimestamp(2000, 1, 1).toSQL
 		for {
 			apply <- Applys.sortBy(_.updated.desc) if apply.stage < Applys.REFUSED && (member || apply.user === user) && (apply.stage > Applys.PENDING || promoted)
@@ -58,28 +59,46 @@ object Applys extends TableQuery(new Applys(_)) with PubSub[User] {
 	})
 
 	// Update the read state flag
-	def markAsRead(id: Int, user: User): Unit = {
-		val query = ApplyReadStates insertOrUpdate ApplyReadState(user.id, id, SmartTimestamp.now)
-		query.run foreach {
-			_ => publish(UnreadUpdated(id, false), u => u.id == user.id)
-		}
+	def markAsRead(user: User, apply_id: Int): Unit = {
+		val query = ApplyReadStates insertOrUpdate ApplyReadState(user.id, apply_id, SmartTimestamp.now)
+		for (_ <- query.run) publish(UnreadUpdated(apply_id, false), u => u.id == user.id)
 	}
+
+	// Return messages in the discussion feed
+	val feedForApply = Compiled((apply: Rep[Int], with_secret: Rep[Boolean]) => {
+		ApplyFeed.filter(m => m.apply === apply && (!m.secret || with_secret)).sortBy(m => m.date.asc)
+	})
 
 	// Post a new message in an application
 	def postMessage(sender: User, apply_id: Int, message: String, secret: Boolean = true, system: Boolean = false) = {
 		val now = SmartTimestamp.now.toSQL
 		val msg = ApplyFeedMessage(0, apply_id, sender.id, now, message, secret, system)
 
+		// Attempt to send a private message, but the user is not a member
+		if (secret && !sender.member)
+			throw new Exception("You are not allowed to post private messages on applications")
+
 		val query = (for {
+		// Fetch the application and ensure that the user can access it
 			apply <- Applys.filter(_.id === apply_id).result.head
 			_ = if (canAccess(apply.user, apply.stage, sender)) () else throw new Exception("You are not allowed to access this application")
+
+			// Insert the message in the database
 			msg_id <- ApplyFeed += msg
+
+			// Set have_posts and updated field for the application
 			_ <- Applys.filter(_.id === apply_id).map(a => (a.have_posts, a.updated)).update((true, now))
+
+			// Adjust application object fields to match what we just changed
+			apply_updated = apply.copy(have_posts = true, updated = now)
+
+			// Register the thread as read for the poster
 			_ <- ApplyReadStates insertOrUpdate ApplyReadState(sender.id, apply_id, SmartTimestamp.now)
-			apply_updated <- Applys.filter(_.id === apply_id).result.head
 		} yield (apply_updated, msg_id)).transactionally
 
 		// Check that the user can access the message
+		// User can access a message if he can access the application that contains it and
+		// is member of the message is not secret
 		def canAccessMessage(owner: Int, stage: Int, user: User, secret: Boolean) = {
 			canAccess(owner, stage, user) && (!secret || user.member)
 		}
@@ -103,18 +122,14 @@ class Applys(tag: Tag) extends Table[Apply](tag, "gt_apply") {
 	def have_posts = column[Boolean]("have_posts")
 	def updated = column[Timestamp]("updated")
 
-	def * = (id, user, date, stage, have_posts, updated) <> (Apply.tupled, Apply.unapply)
+	def * = (id, user, date, stage, have_posts, updated) <>(Apply.tupled, Apply.unapply)
 }
 
 // ============================================================================
 
 case class ApplyFeedMessage(id: Int, apply: Int, user: Int, date: Timestamp, text: String, secret: Boolean, system: Boolean)
 
-object ApplyFeed extends TableQuery(new ApplyFeed(_)) {
-	val forApply = Compiled((apply: Rep[Int], with_secret: Rep[Boolean]) => {
-		ApplyFeed.filter(m => m.apply === apply && (!m.secret || with_secret)).sortBy(m => m.date.asc)
-	})
-}
+object ApplyFeed extends TableQuery(new ApplyFeed(_))
 
 class ApplyFeed(tag: Tag) extends Table[ApplyFeedMessage](tag, "gt_apply_feed") {
 	def id = column[Int]("id", O.PrimaryKey)
@@ -125,7 +140,7 @@ class ApplyFeed(tag: Tag) extends Table[ApplyFeedMessage](tag, "gt_apply_feed") 
 	def secret = column[Boolean]("secret")
 	def system = column[Boolean]("system")
 
-	def * = (id, apply, user, date, text, secret, system) <> (ApplyFeedMessage.tupled, ApplyFeedMessage.unapply)
+	def * = (id, apply, user, date, text, secret, system) <>(ApplyFeedMessage.tupled, ApplyFeedMessage.unapply)
 }
 
 // ============================================================================
@@ -139,6 +154,6 @@ class ApplyReadStates(tag: Tag) extends Table[ApplyReadState](tag, "gt_apply_rea
 	def apply = column[Int]("apply", O.PrimaryKey)
 	def date = column[Timestamp]("date")
 
-	def * = (user, apply, date) <> (ApplyReadState.tupled, ApplyReadState.unapply)
+	def * = (user, apply, date) <>(ApplyReadState.tupled, ApplyReadState.unapply)
 }
 
