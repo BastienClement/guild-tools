98b1e74553153b0dc3066cc12e7e1246c2627feb
Bastien Clément <g@ledric.me>
2015-11-02 20:01:29 +0100

    Deny application access if no char is registered

--- app/controllers/webtools/ApplicationController.scala
+++ app/controllers/webtools/ApplicationController.scala
@@ -1,12 +1,14 @@
 package controllers.webtools
 
 import controllers.WebTools
+import controllers.WebTools.Deny
 import play.api.mvc.Action
 
 trait ApplicationController {
 	this: WebTools =>
 
 	def dispatch = UserAction { req =>
+		if (req.chars.isEmpty) throw Deny
 		req.session.data match {
 			case session if session.contains("charter") => Redirect("/wt/application/step2")
 			case _ => Redirect("/wt/application/step1")
@@ -14,6 +16,7 @@ trait ApplicationController {
 	}
 
 	def step1 = UserAction { req =>
+		if (req.chars.isEmpty) throw Deny
 		if (req.getQueryString("validate").isDefined) {
 			Redirect("/wt/application/step2").withSession(req.session + ("charter" -> "1"))
 		} else {
@@ -22,6 +25,7 @@ trait ApplicationController {
 	}
 
 	def step2 = UserAction { req =>
+		if (req.chars.isEmpty) throw Deny
 		Ok(views.html.wt.application.step2.render(req))
 	}
 
