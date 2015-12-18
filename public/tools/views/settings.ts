9511bac4fadddf1f815b8b4aa582903ebb4b29d0
galedric <g@ledric.me>
2015-12-18 15:12:40 +0100

    Settings require checkboxes and labels

--- public/tools/views/settings.ts
+++ public/tools/views/settings.ts
@@ -1,7 +1,7 @@
 import { Element, Dependencies, PolymerElement, Inject, Property, PolymerModelEvent } from "elements/polymer";
 import { View } from "elements/app";
 import { GtBox } from "elements/box";
-import { GtButton } from "elements/widgets";
+import { GtButton, GtCheckbox, GtLabel } from "elements/widgets";
 import { GtDialog } from "elements/dialog";
 import { Server } from "client/server";
 import { User } from "services/roster";
@@ -10,7 +10,7 @@ type SettingsTab = [string, string, string];
 
 @View("settings", () => [{ title: "Settings", link: "/settings", active: true }])
 @Element("gt-settings", "/assets/views/settings.html")
-@Dependencies(GtBox, GtButton, GtDialog)
+@Dependencies(GtBox, GtButton, GtDialog, GtCheckbox, GtLabel)
 export class GtSettings extends PolymerElement {
 	private tabs: SettingsTab[] = [
 		["General", "settings", "general"],
