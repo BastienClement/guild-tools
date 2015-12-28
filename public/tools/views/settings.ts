006059f5fb34d42b87fe412ec6304375a5b09bb5
Bastien Clément <g@ledric.me>
2015-12-29 00:05:08 +0100

    Settings page

--- public/tools/views/settings.ts
+++ public/tools/views/settings.ts
@@ -13,16 +13,28 @@ type SettingsTab = [string, string, string];
 @Dependencies(GtBox, GtButton, GtDialog, GtCheckbox, GtLabel)
 export class GtSettings extends PolymerElement {
 	private tabs: SettingsTab[] = [
-		["General", "settings", "general"],
-		["Notifications", "notifications", "notifications"],
-		["Game", "folder", "game"],
+		//["General", "settings", "general"],
+		//["Notifications", "notifications", "notifications"],
+		//["Game", "folder", "game"],
 		["Advanced", "build", "advanced"]
 	];
 
-	private tab = "general";
+	private tab = "advanced";
 
 	private SelectTab(ev: PolymerModelEvent<SettingsTab>) {
 		let tab = ev.model.item;
 		this.tab = tab[2];
+	}
+
+	// Advanced
+	private faster_loading = localStorage.getItem("loading.fast") === "1";
+	private verbose_gtp3 = localStorage.getItem("socket.verbose") === "1";
+	private sourcemap_fix = localStorage.getItem("traceur.sourcemap.fix") === "1";
+	private streams_zerolatency = localStorage.getItem("streams.zerolatency") === "1";
+
+	private ToggleLocalStorage(ev: Event) {
+		let target = <GtCheckbox & HTMLElement> ev.target;
+		let key = target.getAttribute("key");
+		localStorage.setItem(key, target.checked ? "1" : "0");
 	}
 }
