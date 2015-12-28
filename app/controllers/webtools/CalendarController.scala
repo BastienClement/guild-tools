4031627fc784887fb8705a54833846c1ad1173d8
Bastien Clément <g@ledric.me>
2015-12-29 00:57:56 +0100

    JsonP api

--- app/controllers/webtools/CalendarController.scala
+++ app/controllers/webtools/CalendarController.scala
@@ -8,21 +8,25 @@ import scala.concurrent.Future
 
 class CalendarController extends Controller with WtController {
 	def unread_events = UserAction.async { req =>
-		if (req.user.roster) {
-			val query =
-				sql"""
-				SELECT COUNT(*)
-				FROM gt_events AS e
-				JOIN phpbb_users AS u ON u.user_id = ${req.user.id }
-				LEFT JOIN gt_answers AS a ON e.id = a.event AND a.user = u.user_id
-				WHERE e.date >= NOW() AND e.garbage = 0 AND (
-					(e.type IN (1, 2, 4) AND a.answer IS NULL)
-		         OR (e.type = 3 AND a.answer = 0)
-		      )
-			"""
-			query.as[Int].head.run.map(count => Ok(count.toString))
-		} else {
-			Future.successful(Ok("0"))
-		}
+		val count =
+			if (req.user.roster) {
+				val query =
+					sql"""
+					SELECT COUNT(*)
+					FROM gt_events AS e
+					JOIN phpbb_users AS u ON u.user_id = ${req.user.id }
+					LEFT JOIN gt_answers AS a ON e.id = a.event AND a.user = u.user_id
+					WHERE e.date >= NOW() AND e.garbage = 0 AND (
+						(e.type IN (1, 2, 4) AND a.answer IS NULL)
+			         OR (e.type = 3 AND a.answer = 0)
+			      )
+				"""
+				query.as[Int].head.run
+			} else {
+				Future.successful(0)
+			}
+
+		val callback = req.getQueryString("callback").getOrElse("jsonp")
+		count.map(c => Ok(callback + "(" + c.toString + ")").as("application/javascript"))
 	}
 }
