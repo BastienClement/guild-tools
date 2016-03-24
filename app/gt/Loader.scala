4a0d837ea2dee2c7f06eb0655744853386972073
galedric <g@ledric.me>
2016-03-24 14:30:58 +0100

    Change non-secure wording

--- app/gt/Loader.scala
+++ app/gt/Loader.scala
@@ -2,9 +2,6 @@ package gt
 
 import com.google.inject.AbstractModule
 
-/**
-  * Created by galedric on 20.03.2016.
-  */
 class Loader extends AbstractModule {
 	override def configure(): Unit = {
 		bind(classOf[GuildTools]).asEagerSingleton()
