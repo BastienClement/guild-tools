5bd45a1d3952e775d7374c8043a16b66ed06be72
galedric <g@ledric.me>
2016-04-04 15:52:04 +0200

    What is happening ?

--- app/gt/Loader.scala
+++ app/gt/Loader.scala
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule
 
 class Loader extends AbstractModule {
 	override def configure(): Unit = {
+		println("Configuring")
 		bind(classOf[GuildTools]).asEagerSingleton()
 	}
 }
