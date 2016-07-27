1ec56b1b0141cde8455e667249d85ecb2d7e8b31
Bastien Clément <g@ledric.me>
2016-07-27 18:20:21 +0200

    Deprecate PayloadFlags Utf8Data and JsonData

--- shared/src/main/scala/gtp3/PayloadFlags.scala
+++ shared/src/main/scala/gtp3/PayloadFlags.scala
@@ -3,9 +3,12 @@ package gtp3
 object PayloadFlags {
 	final val None = 0x00
 	final val Compress = 0x01
-	final val Utf8Data = 0x02
-	final val JsonData = 0x04
-	// Require Utf8Data
 	final val PickledData = 0x10
 	final val Ignore = 0x80
+
+	@deprecated("There should be only Pickled data from now on", "7.0")
+	final val Utf8Data = 0x02
+
+	@deprecated("There should be only Pickled data from now on", "7.0")
+	final val JsonData = 0x04
 }
