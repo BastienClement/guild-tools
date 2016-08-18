cc665c79ff35289c59823dca69b157f99fca00c2
Bastien Clément <g@ledric.me>
2016-08-18 23:50:12 +0200

    Add textarea component and normalize label handling

--- client/src/gt/component/widget/form/GtCheckbox.scala
+++ client/src/gt/component/widget/form/GtCheckbox.scala
@@ -1,8 +1,10 @@
 package gt.component.widget.form
 
 import gt.component.GtHandler
+import gt.component.widget.form.GtCheckbox.AnyZero
 import org.scalajs.dom.MouseEvent
 import rx.{Obs, Var}
+import util.Zero
 import util.implicits._
 import util.jsannotation.js
 import xuen.Component
@@ -10,7 +12,11 @@ import xuen.Component
 object GtCheckbox extends Component[GtCheckbox](
 	selector = "gt-checkbox",
 	templateUrl = "/assets/imports/widgets.html"
-)
+) {
+	implicit val AnyZero = new Zero[Any] {
+		val zero: Any = null
+	}
+}
 
 @js class GtCheckbox extends GtHandler with Interactive {
 	/** Disables the input */
@@ -18,21 +24,27 @@ object GtCheckbox extends Component[GtCheckbox](
 	this.dyn.disabled = attribute[Boolean].dyn
 	@inline def _disabled: Var[Boolean] = this.dyn.disabled.asInstanceOf[Var[Boolean]]
 
+	/** Input label */
+	val label = attribute[String]
+
 	/**
 	  * If defined, the checkbox act as a radio button
 	  * If the radio button is selected, this attribute
 	  * will be reflected to the value model.
 	  */
-	val radio = attribute[String]
+	val radio = property[Any] := null
 
 	/** Current checkbox state */
 	val checked = model[Boolean]
 
 	/** Proxy to input value */
-	val value = model[String]
+	val value = model[Any]
 
+	override def ready(): Unit = {
+		if (hasAttribute("radio")) radio := getAttribute("radio")
+	}
 
-	checked ~> Obs { if (checked && radio.! != null) value := radio }
+	checked ~> Obs { if (checked && radio.! != null) value := radio.! }
 	private val sync = Obs { checked := (radio.! == value.!) }
 	value ~> sync
 	radio ~> sync
