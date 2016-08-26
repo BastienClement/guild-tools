19c6b970160ac4e69da5faf6cc22fbde31598861
Bastien Clément <g@ledric.me>
2016-08-26 21:46:27 +0200

    Update Specs file

--- shared/shared/src/main/scala/data/Specializations.scala
+++ shared/shared/src/main/scala/data/Specializations.scala
@@ -3,80 +3,80 @@ package data
 import util.annotation.data
 
 object Specializations {
-	@data case class Spec(id: Int, clss: Int, name: String, role: String, icon: String, inactive: Boolean = false)
-
 	// Roles aliases
 	private final val DPS = "DPS"
 	private final val HEALING = "HEALING"
 	private final val TANK = "TANK"
 
-	/** Constructs dummy specialization */
-	private def dummy(id: Int, clss: Int = 0): Spec = Spec(id, clss, "Unknown", "UNKNOW", "")
-
 	/** Specializations data */
-	private val specs = IndexedSeq(
-		dummy(0),
+	private var specs = Vector.empty[Spec]
 
-		// Warrior
-		Spec(1, 1, "Arms", DPS, "ability_warrior_savageblow"),
-		Spec(2, 1, "Fury", DPS, "ability_warrior_innerrage"),
-		Spec(3, 1, "Protection", TANK, "ability_warrior_defensivestance"),
+	@data case class Spec(clss: Int, name: String, role: String, icon: String) {
+		val id = specs.size
+		specs = specs :+ this
+	}
 
-		// Paladin
-		Spec(4, 2, "Holy", HEALING, "spell_holy_holybolt"),
-		Spec(5, 2, "Protection", TANK, "ability_paladin_shieldofthetemplar"),
-		Spec(6, 2, "Retribution", DPS, "spell_holy_auraoflight"),
+	val Dummy = Spec(0, "Unknown", "UNKNOW", "")
 
-		// Hunter
-		Spec(7, 3, "Beast Mastery", DPS, "ability_hunter_bestialdiscipline"),
-		Spec(8, 3, "Marksmanship", DPS, "ability_hunter_focusedaim"),
-		Spec(9, 3, "Survival", DPS, "ability_hunter_camouflage"),
+	// Warrior
+	val Arms = Spec(1, "Arms", DPS, "ability_warrior_savageblow")
+	val Fury = Spec(1, "Fury", DPS, "ability_warrior_innerrage")
+	val ProtectionWarrior = Spec(1, "Protection", TANK, "ability_warrior_defensivestance")
 
-		// Rogue
-		Spec(10, 4, "Assassination", DPS, "ability_rogue_deadlybrew"),
-		Spec(11, 4, "Outlaw", DPS, "inv_sword_30"),
-		Spec(12, 4, "Subtlety", DPS, "ability_stealth"),
+	// Paladin
+	val HolyPaladin = Spec(2, "Holy", HEALING, "spell_holy_holybolt")
+	val ProtectionPaladin = Spec(2, "Protection", TANK, "ability_paladin_shieldofthetemplar")
+	val Retribution = Spec(2, "Retribution", DPS, "spell_holy_auraoflight")
 
-		// Priest
-		Spec(13, 5, "Discipline", HEALING, "spell_holy_powerwordshield"),
-		Spec(14, 5, "Holy", HEALING, "spell_holy_guardianspirit"),
-		Spec(15, 5, "Shadow", DPS, "spell_shadow_shadowwordpain"),
+	// Hunter
+	val BeastMastery = Spec(3, "Beast Mastery", DPS, "ability_hunter_bestialdiscipline")
+	val Marksmanship = Spec(3, "Marksmanship", DPS, "ability_hunter_focusedaim")
+	val Survival = Spec(3, "Survival", DPS, "ability_hunter_camouflage")
 
-		// Death Knight
-		Spec(16, 6, "Blood", TANK, "spell_deathknight_bloodpresence"),
-		Spec(17, 6, "Frost", DPS, "spell_deathknight_frostpresence"),
-		Spec(18, 6, "Unholy", DPS, "spell_deathknight_unholypresence"),
+	// Rogue
+	val Assassination = Spec(4, "Assassination", DPS, "ability_rogue_deadlybrew")
+	val Outlaw = Spec(4, "Outlaw", DPS, "inv_sword_30")
+	val Subtlety = Spec(4, "Subtlety", DPS, "ability_stealth")
 
-		// Shaman
-		Spec(19, 7, "Elemental", DPS, "spell_nature_lightning"),
-		Spec(20, 7, "Enhancement", DPS, "spell_shaman_improvedstormstrike"),
-		Spec(21, 7, "Restoration", HEALING, "spell_nature_magicimmunity"),
+	// Priest
+	val Discipline = Spec(5, "Discipline", HEALING, "spell_holy_powerwordshield")
+	val HolyPriest = Spec(5, "Holy", HEALING, "spell_holy_guardianspirit")
+	val Shadow = Spec(5, "Shadow", DPS, "spell_shadow_shadowwordpain")
 
-		// Mage
-		Spec(22, 8, "Arcane", DPS, "spell_holy_magicalsentry"),
-		Spec(23, 8, "Fire", DPS, "spell_fire_firebolt02"),
-		Spec(24, 8, "Frost", DPS, "spell_frost_frostbolt02"),
+	// Death Knight
+	val Blood = Spec(6, "Blood", TANK, "spell_deathknight_bloodpresence")
+	val FrostDk = Spec(6, "Frost", DPS, "spell_deathknight_frostpresence")
+	val Unholy = Spec(6, "Unholy", DPS, "spell_deathknight_unholypresence")
 
-		// Warlock
-		Spec(25, 9, "Affliction", DPS, "spell_shadow_deathcoil"),
-		Spec(26, 9, "Demonology", DPS, "spell_shadow_metamorphosis"),
-		Spec(27, 9, "Destruction", DPS, "spell_shadow_rainoffire"),
+	// Shaman
+	val Elemental = Spec(7, "Elemental", DPS, "spell_nature_lightning")
+	val Enhancement = Spec(7, "Enhancement", DPS, "spell_shaman_improvedstormstrike")
+	val RestorationShaman = Spec(7, "Restoration", HEALING, "spell_nature_magicimmunity")
 
-		// Monk
-		Spec(28, 10, "Brewmaster", TANK, "spell_monk_brewmaster_spec"),
-		Spec(29, 10, "Mistweaver", HEALING, "spell_monk_mistweaver_spec"),
-		Spec(30, 10, "Windwalker", DPS, "spell_monk_windwalker_spec"),
+	// Mage
+	val Arcane = Spec(8, "Arcane", DPS, "spell_holy_magicalsentry")
+	val Fire = Spec(8, "Fire", DPS, "spell_fire_firebolt02")
+	val FrostMage = Spec(8, "Frost", DPS, "spell_frost_frostbolt02")
 
-		// Druid
-		Spec(31, 11, "Balance", DPS, "spell_nature_starfall"),
-		Spec(32, 11, "Feral", DPS, "ability_druid_catform"),
-		Spec(33, 11, "Guardian", TANK, "ability_racial_bearform"),
-		Spec(34, 11, "Restoration", HEALING, "spell_nature_healingtouch"),
+	// Warlock
+	val Affliction = Spec(9, "Affliction", DPS, "spell_shadow_deathcoil")
+	val Demonology = Spec(9, "Demonology", DPS, "spell_shadow_metamorphosis")
+	val Destruction = Spec(9, "Destruction", DPS, "spell_shadow_rainoffire")
 
-		// Demon Hunter
-		Spec(35, 12, "Havoc", DPS, "ability_demonhunter_specdps"),
-		Spec(36, 12, "Vengeance", TANK, "ability_demonhunter_spectank")
-	)
+	// Monk
+	val Brewmaster = Spec(10, "Brewmaster", TANK, "spell_monk_brewmaster_spec")
+	val Mistweaver = Spec(10, "Mistweaver", HEALING, "spell_monk_mistweaver_spec")
+	val Windwalker = Spec(10, "Windwalker", DPS, "spell_monk_windwalker_spec")
+
+	// Druid
+	val Balance = Spec(11, "Balance", DPS, "spell_nature_starfall")
+	val Feral = Spec(11, "Feral", DPS, "ability_druid_catform")
+	val Guardian = Spec(11, "Guardian", TANK, "ability_racial_bearform")
+	val RestorationDruid = Spec(11, "Restoration", HEALING, "spell_nature_healingtouch")
+
+	// Demon Hunter
+	val Havoc = Spec(12, "Havoc", DPS, "ability_demonhunter_specdps")
+	val Vengeance = Spec(12, "Vengeance", TANK, "ability_demonhunter_spectank")
 
 	/** Specializations by class */
 	private val byClass = specs.groupBy(s => s.clss)
